eb41101dde5c61fd40383dbf3b20d9404c2849b1
Philippe Moore <philippealmoore@gmail.com>
2017-09-21 18:47:19 -0700

    sample firebase file

---
+++ src/samplefirebase.js
@@ -0,0 +1,11 @@
+  // Initialize Firebase
+  var sample_config = {
+    apiKey: 'YOURAPIKEY',
+    authDomain: 'YOURAUTHDOMAIN',
+    databaseURL: 'YOURDBURL',
+    projectId: 'YOURPROJECID',
+    storageBucket: 'YOURSTORAGEBUCKET',
+    messagingSenderId: 'YOURMESSAGINGSENDERID'
+  }
+
+  export default sample_config
